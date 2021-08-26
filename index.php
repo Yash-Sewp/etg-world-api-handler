8bfb9995d8cca63f98181670ba52b69825a43730
Yash <yashveersewparthab@gmail.com>
2021-08-26 17:48:59 +0200

    email func

--- index.php
+++ index.php
@@ -3,25 +3,32 @@
     // header("Content-Type: application/json; charset=UTF-8");
     // header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-    echo 'Hello ' . htmlspecialchars($_GET["name"]) . '!';
+    // echo 'Hello ' . htmlspecialchars($_GET["name"]) . '!';
 
-    $to = "yashveersewparthab@gmail.com";
-    $subject = "This is subject";
-    
-    $message = "<b>This is HTML message.</b>";
-    $message .= "<h1>This is headline.</h1>";
-    
-    $header = "From:abc@somedomain.com \r\n";
-    $header .= "Cc:afgh@somedomain.com \r\n";
-    $header .= "MIME-Version: 1.0\r\n";
-    $header .= "Content-type: text/html\r\n";
-    
-    $retval = mail ($to,$subject,$message,$header);
-    
-    if( $retval == true ) {
-       echo "Message sent successfully...";
-    }else {
-       echo "Message could not be sent...";
+    $to = 'yashveersewparthab@gmail.com';
+    $subject = 'Marriage Proposal';
+    $from = 'peterparker@email.com';
+     
+    // To send HTML mail, the Content-type header must be set
+    $headers  = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+     
+    // Create email headers
+    $headers .= 'From: '.$from."\r\n".
+        'Reply-To: '.$from."\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+     
+    // Compose a simple HTML email message
+    $message = '<html><body>';
+    $message .= '<h1 style="color:#f40;">Hi Jane!</h1>';
+    $message .= '<p style="color:#080;font-size:18px;">Will you marry me?</p>';
+    $message .= '</body></html>';
+     
+    // Sending email
+    if(mail($to, $subject, $message, $headers)){
+        echo 'Your mail has been sent successfully.';
+    } else{
+        echo 'Unable to send email. Please try again.';
     }
 
 // http://127.0.0.1/projects/etg-world-api-handler/?name=Something
